5c2f62646298ebb69bbbbd7f854a3a26aa4522e8
Andrea Castro <ap.castrocastellon@gmail.com>
2021-09-18 13:16:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ul>
+      <li><router-link :to="{name: 'inicio'}">Inicio</router-link></li>
+    </ul>
+    <transition name="vista">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style scoped>
+
+.vista-enter-active, .vista-leave-active {
+    transition: opacity .1s;
+}
+.vista-enter, .vista-leave-to{
+    opacity: 0;
+}
+</style>
